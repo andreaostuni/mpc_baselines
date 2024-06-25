e1e342175d59e68d8f9db19c3b0bad1babb9145c
Andrea Ostuni <andrea.ostuni@polito.it>
2024-06-25 18:31:27 +0200

    refactor cleaning prints

--- mpc_baselines/dynamics/pendulum.py
+++ mpc_baselines/dynamics/pendulum.py
@@ -9,7 +9,6 @@ class PendulumDynamics(torch.nn.Module):
        def forward(self, state, action):
               th = state[:, 0].view(-1, 1)
               thdot = state[:, 1].view(-1, 1)
-              # print(f'th {th}, thdot {thdot}')
 
               g = 10
               m = 1.0
@@ -24,17 +23,13 @@ class PendulumDynamics(torch.nn.Module):
               newthdot = torch.clamp(newthdot, -8, 8)
 
               state = torch.cat((angle_normalize(newth), newthdot), dim=1)
-              # print(f'state {state}')
               return state
        
        def grad_input(self, state, action):
               assert isinstance(state, Variable) == isinstance(action, Variable)
-              # print(f'state {state}, action {action}')
               th = state[:, 0]
-              # print(f'th {th}')
 
               x_dim, u_dim = state.ndimension(), action.ndimension()
-              # print(f'x_dim {x_dim}, u_dim {u_dim}')
               n_batch, n_state = state.size()
               _, n_ctrl = action.size()
 
